55c0da3d41aff53e271e131be5fd45bf1175ccfd
liubomr_sliusar <l1l2369100@gmail.com>
2024-10-09 18:18:34 +0300

    feat: added new details for Product interface

--- src/shared/types/Product.ts
+++ src/shared/types/Product.ts
@@ -5,9 +5,44 @@ export interface Product {
   price: number;
   discount: number;
   imageUrl: string;
-  category: "notebook" | "t-shirt" | "mug";
+  category: Category;
   /** Quantity of the product in stock */
   stock: number;
-  colors?: string[];
-  sizes?: string[];
+  colors?: Colors[];
+  sizes?: Sizes[];
 }
+
+// export type Colors = "white" | "grey" | "brown" | "navy" | "black" | "blue" | "silver" | "red" | "green" | "yellow" | "pink";
+export const colors = [
+  "white", //
+  "grey",
+  "brown",
+  "navy",
+  "black",
+  "blue",
+  "silver",
+  "red",
+  "green",
+  "yellow",
+  "pink",
+] as const;
+export type Colors = (typeof colors)[number];
+// export type Sizes = "S" | "M" | "L" | "XL" | "XXL" | "XXXL" | "A4" | "A5";
+export const sizes = [
+  "S", //
+  "M",
+  "L",
+  "XL",
+  "XXL",
+  "XXXL",
+  "A4",
+  "A5",
+] as const;
+export type Sizes = (typeof sizes)[number];
+// export type Category = "notebook" | "t-shirt" | "mug";
+export const categories = [
+  "notebook", //
+  "t-shirt",
+  "mug",
+] as const;
+export type Category = (typeof categories)[number];
